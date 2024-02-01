53722917d231ac33a4bd98bdda99bd3276193b70
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2024-02-01 17:41:56 -0500

    update test

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,7 +1,8 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import App from '../App';
 import { render, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import App from '../App';
+
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
@@ -38,27 +39,40 @@ defineFeature(feature, (test) => {
     then,
   }) => {
     let AppComponent;
+    let eventList;
     given('the user has events displayed', async () => {
       AppComponent = render(<App />);
       const AppDOM = AppComponent.container.firstChild;
       await waitFor(() => {
-        const eventList = within(AppDOM).queryAllByRole('listitem');
+        eventList = within(AppDOM).queryAllByRole('listitem');
         expect(eventList[0]).toBeTruthy();
       });
     });
 
-    when(
-      'the user chooses to change the number of events displayed',
-      async () => {
-        const button = AppComponent.queryByTestId('numberOfEventsInput');
+    let AppDOM;
+    let NumberOfEventsDOM;
+    let numberOfEventsInput;
+    when('the user chooses to change the number of events displayed', async () => {
+      AppComponent = render(<App />);
+      const user = userEvent.setup();
+      AppDOM = AppComponent.container.firstChild;
+      NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+      numberOfEventsInput = within(NumberOfEventsDOM).queryByRole('textbox');
+      await user.click(numberOfEventsInput, '{backspace}{backspace}10');
 
-        await userEvent.click(button, '{backspace}{backspace}10');
-      }
-    );
+    });
 
-    then(
-      'the number of events displayed will update to the number the user selected',
-      () => {
+    // when(
+    //   'the user chooses to change the number of events displayed',
+    //   async () => {
+    //     const user = userEvent.setup();
+    //     const button = AppComponent.queryByTestId('numberOfEventsInput');
+        
+    //     await user.type(button, "{backspace}{backspace}3");
+    //   }
+    // );
+
+    then('the number of events displayed will update to the number the user selected', async () => {
         const AppDOM = AppComponent.container.firstChild;
         const eventList = within(AppDOM).queryAllByRole('listitem');
         expect(eventList.length).toEqual(32);
