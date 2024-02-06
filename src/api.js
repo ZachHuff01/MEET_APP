efdda8cfcacfadb87c6d712d5dcfdbb80cda3e57
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2024-02-06 18:16:05 -0500

    added line NProgress.done();

--- src/api.js
+++ src/api.js
@@ -62,7 +62,7 @@ export const getEvents = async () => {
 
   if ( !navigator.onLine ) {
     const events = localStorage.getItem("lastEvents");
-    
+    NProgress.done();
     return events?JSON.parse(events):[];
   }
 
@@ -74,6 +74,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
