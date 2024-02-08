5830d85ebc5328205858ebaacc3a13cd4ac06a3f
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2024-02-08 13:42:13 -0500

    change names on chart to be visible

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -9,7 +9,7 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
       const newData = allLocations.map((location) => {
         const count = events.filter((event) => event.location === location).length
-        const city = location.split(', ')[0]
+        const city = location.split(/,| -/)[0]
         return { city, count };
       })
       return newData;
@@ -27,8 +27,8 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 60,
+          left: -30,
         }}
       >
         <CartesianGrid />
